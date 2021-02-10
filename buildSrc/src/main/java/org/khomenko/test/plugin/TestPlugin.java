914560f6fa8c276c5d03b336bc783fa2f6a0b86a
Roman Khomenko <romankhomenko1995@gmail.com>
2021-02-10 12:40:56 +0300

    Bug demo

--- buildSrc/src/main/java/org/khomenko/test/plugin/TestPlugin.java
+++ buildSrc/src/main/java/org/khomenko/test/plugin/TestPlugin.java
@@ -1,7 +1,10 @@
 package org.khomenko.test.plugin;
 
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.tasks.TaskContainer;
 
 import javax.annotation.Nonnull;
 
@@ -9,5 +12,8 @@ public class TestPlugin implements Plugin<Project> {
     @Override
     public void apply(@Nonnull Project project) {
         project.getLogger().warn("It's work!");
+
+        TaskContainer tasks = project.getTasks();
+        Task task = tasks.create("test-task", TestTask.class);
     }
 }
---
+++ buildSrc/src/main/java/org/khomenko/test/plugin/TestTask.java
@@ -0,0 +1,19 @@
+package org.khomenko.test.plugin;
+
+import org.apache.tools.ant.taskdefs.Java;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.JavaExec;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.TaskContainer;
+
+public class TestTask extends DefaultTask {
+    @TaskAction
+    void run() {
+        var log = getLogger();
+        log.warn("Task started!");
+
+        TaskContainer tasks = getProject().getTasks();
+        JavaExec bootRun = (JavaExec) tasks.findByName("bootRun");
+        bootRun.getCommandLine().stream().forEach(l -> log.warn(l));
+    }
+}
